917ee79e5a40a7fd1b0faf35052b5cbef496d2f5
tiredhaydn <tiredhaydn@gmail.com>
2021-10-04 22:09:32 +0900

    add comments

--- src/bin/problem002/main.rs
+++ src/bin/problem002/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let mut answer = 0;
-    let mut fib = (1, 2);
-    while fib.0 <= 400_0000 {
+    let mut fib = (1, 2);               // (odd, even)
+    while fib.1 <= 400_0000 {
         answer += fib.1;
-        fib = (fib.1, fib.0 + fib.1);
-        fib = (fib.1, fib.0 + fib.1);
-        fib = (fib.1, fib.0 + fib.1);
+        fib = (fib.1, fib.0 + fib.1);       // (even, odd)
+        fib = (fib.1, fib.0 + fib.1);       // (odd, odd)
+        fib = (fib.1, fib.0 + fib.1);       // (odd, even)
     }
     println!("{}", answer);
 }
