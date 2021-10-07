4ca6a3229ff47c312a495edc38209ebb9d7ae3e1
tiredhaydn <tiredhaydn@gmail.com>
2021-10-07 10:53:37 +0900

    add another solution

--- src/bin/problem001/main.rs
+++ src/bin/problem001/main.rs
@@ -7,3 +7,15 @@ fn main() {
     }
     println!("{}", answer);
 }
+
+
+fn sum(m: i32, limit: i32) -> i32 {
+    let n = (limit - 1) / m;
+    n * (n + 1) / 2 * m
+}
+
+fn another_solution() {
+    const LIMIT: i32 = 1000;
+    let answer = sum(3, LIMIT) + sum(5, LIMIT) - sum(15, LIMIT);
+    println!("{}", answer);
+}
