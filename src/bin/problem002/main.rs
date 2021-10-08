5d7fb8278a6d73843654aa02863816c470445ce9
tiredhaydn <tiredhaydn@gmail.com>
2021-10-08 15:58:17 +0900

    add comment

--- src/bin/problem002/main.rs
+++ src/bin/problem002/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut answer = 0;
     let mut fib = (1, 2);               // (odd, even)
     while fib.1 <= 400_0000 {
-        answer += fib.1;
+        answer += fib.1;                    // add even number
         fib = (fib.1, fib.0 + fib.1);       // (even, odd)
         fib = (fib.1, fib.0 + fib.1);       // (odd, odd)
         fib = (fib.1, fib.0 + fib.1);       // (odd, even)
